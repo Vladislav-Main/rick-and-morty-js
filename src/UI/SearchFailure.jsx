f3e92a87c18d3ca0162c09da2ff06e8021b7e516
Vladislav-Main <vlados.77772@gmail.com>
2021-08-18 10:10:00 +0300

    one change has been added to word in searchfailure.jsx

--- src/UI/SearchFailure.jsx
+++ src/UI/SearchFailure.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const SearchFailure = () => {
   return (
     <div>
-      <h2>Nothing was found. Change filters too see more.</h2>
+      <h2>Nothing was found. Change filters to see more.</h2>
     </div>
   )
 }
